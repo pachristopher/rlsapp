82c7ed81a14858546de8130101127b75d0eee30e
pachristopher <paulchristopher13@gmail.com>
2023-04-17 23:59:31 +0100

    re-did app to make num of family members dynamic

--- newapp.py
+++ newapp.py
@@ -1,34 +1,60 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, make_response
 
 newapp = Flask(__name__)
 
+# Config the app to use a MongoDB database
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 db = client['rls']
 collection = db['users']
 
-@newapp.route('/form', methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
-        address = request.form['address']
-        family = []
-        num_family_members = int(request.form['num_family_members'])
-        for i in range(num_family_members):
-            family_member = {}
-            family_member['name'] = request.form.get('family_name_' + str(i))
-            family_member['age'] = request.form.get('family_age_' + str(i))
-            family_member['reln'] = request.form.get('family_reln_' + str(i))
-            family.append(family_member)
-        collection.insert_one({
-            'name': name,
-            'age': age,
-            'address': address,
-            'family': family
-        })
-        return redirect(url_for('form'))
-    return render_template('form.html', num_family_members=1)
+# Define a list of nationalities for the dropdown
+with open('static/country_list.txt', 'r') as f:
+    nationalities = [country.strip() for country in f.readlines()]
 
+# Create a route to handle client info form 
+@newapp.route('/form', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get client info form form
+        name = request.form.get('name')
+        per_id = request.form.get('per_id')
+        address = request.form.get('address')
+        religion = request.form.get('religion')
+        nationality = request.form.get('nationality')
+        num_fam_mems = int(request.form.get('num_fam_mems'))
+
+        # Create a list to store family member info
+        fam_mems = []
+
+        # Loop through the no of amily members and get their info
+        for mem in range(num_fam_mems):
+            fam_mem = {}
+            fam_mem['name'] = request.form.get('fam_name_' + str(mem))
+            fam_mem['per_id'] = request.form.get('fam_per_id_' + str(mem))
+            fam_mem['address'] = request.form.get('fam_address' + str(mem))
+            fam_mems.append(fam_mem)
+
+        # Insert client info into db
+        client_data = {
+            'name': name,
+            'per_id': per_id,
+            'address': address,
+            'religion': religion,
+            'nationality': nationality,
+            'fam_mems': fam_mems,
+        }
+
+        result = collection.insert_one(client_data)
+
+        # Redirect to success page
+        return render_template('success.html')
+
+    # If the request method is GET, render the form
+    else:
+        # Render the form template with the nationalities dropdown and default number of family members
+        return render_template('form.html', nationalities=nationalities, num_fam_mems=0)
+
+# Run the app
 if __name__ == '__main__':
     newapp.run(port=9003, debug=True)
