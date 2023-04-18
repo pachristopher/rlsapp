77e9eea6f1d49613127ef88af4803a85fbee08f9
pachristopher <paulchristopher13@gmail.com>
2023-04-18 22:30:22 +0100

    added more data fields

--- newapp.py
+++ newapp.py
@@ -8,7 +8,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['rls']
 collection = db['users']
 
-# Define the options for the nationalities field 
+# Define the options for the nationality field 
 with open('static/country_list.txt', 'r') as f:
     NAT_OPTIONS = [line.strip() for line in f.readlines()]
 
@@ -21,33 +21,57 @@ with open('static/languages.txt', 'r') as f:
 def index():
     if request.method == 'POST':
         # Get client info from form
-        name = request.form.get('name')
-        per_id = request.form.get('per_id')
+        rlsno = request.form.get('rlsno')
+        ipo_no = request.form.get('ipo_no')
+        solicitor = request.form.get('solicitor')
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        alias = request.form.get('alias')
+        dob = request.form.get('dob')
+        gender = request.form.get('gender')
         address = request.form.get('address')
-        religion = request.form.get('religion')
+        email = request.form.get('email')
+        passport = request.form.get('passport')
         nationality = request.form.get('nationality')
+        ethnicity = request.form.get('ethnicity')
+        religion = request.form.get('religion')
         language = request.form.get('language')
+        married = request.form.get('married')
         num_fam_mems = int(request.form.get('num_fam_mems'))
 
         # Create a list to store family member info
         fam_mems = []
 
-        # Loop through the no of amily members and get their info
+        # Loop through the number of family members and get their info
         for mem in range(num_fam_mems):
             fam_mem = {}
             fam_mem['name'] = request.form.get('fam_name_' + str(mem))
-            fam_mem['per_id'] = request.form.get('fam_per_id_' + str(mem))
+            fam_mem['ipo_no'] = request.form.get('fam_ipo_no_' + str(mem))
             fam_mem['address'] = request.form.get('fam_address' + str(mem))
+            fam_mem['dob'] = request.form.get('fam_dob_' + str(mem))
+            fam_mem['gender'] = request.form.get('fam_gender_' + str(mem))
+            fam_mem['relnship'] = request.form.get('fam_relnship_' + str(mem))
+            fam_mem['nationality'] = request.form.get('fam_nationality_' + str(mem))
             fam_mems.append(fam_mem)
 
         # Insert client info into db
         client_data = {
-            'name': name,
-            'per_id': per_id,
+            'rlsno': rlsno,
+            'ipo_no': ipo_no,
+            'solicitor': solicitor,
+            'fname': fname,
+            'lname': lname,
+            'alias': alias,
+            'dob': dob,
+            'gender': gender,
             'address': address,
-            'religion': religion,
+            'email': email,
+            'passport': passport,
             'nationality': nationality,
+            'ethnicity': ethnicity,
+            'religion': religion,
             'language': language,
+            'married': married,
             'fam_mems': fam_mems,
         }
 
